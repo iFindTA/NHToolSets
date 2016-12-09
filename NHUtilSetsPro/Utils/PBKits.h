438008c5c5d2bd5a74318ea95a68624d88f5e9fa
hujiaju <hujiaju@hzflk.com>
2016-12-09 10:02:21 +0800

    add back delay method

--- NHUtilSetsPro/Utils/PBKits.h
+++ NHUtilSetsPro/Utils/PBKits.h
@@ -55,6 +55,7 @@ dispatch_async(dispatch_get_main_queue(),block);\
 }
 #define PBMAINDelay(x, block) dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(x * NSEC_PER_SEC)), dispatch_get_main_queue(), block)
 #define PBBACK(block)  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
+#define PBBACKDelay(x, block) dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(x * NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
 
 #ifndef PBFormat
 #define PBFormat(format, ...) [NSString stringWithFormat:format, ##__VA_ARGS__]
